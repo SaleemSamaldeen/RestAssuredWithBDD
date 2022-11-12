50e971eca27d19f5b4b9c5bf1ca9a3aeaea80c74
Saleem <saleemsamaldeen@gmail.com>
2022-11-12 06:08:48 +0530

    Initial commit

---
+++ src/main/java/stepDefinitions/BaseClass.java
@@ -0,0 +1,67 @@
+package stepDefinitions;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class BaseClass {
+
+    private Response response;
+    private RequestSpecification requestSpec;
+    public String baseURI;
+
+    public CarTypes getCarMainTypes(String pathParameter) throws Exception {
+        response = getResponse(pathParameter);
+        try {
+            return new CarTypes(response.jsonPath().get("page"), response.jsonPath().get("pageSize"),
+                    response.jsonPath().get("totalPageCount"), response.jsonPath().get("wkda"));
+        } catch (ClassCastException e) {
+            throw new Exception("Incorrect data displayed while trying to fetch Car main types using parameter " + pathParameter);
+        }
+    }
+
+    public BuiltYear getCarBuiltYears(String pathParameter) throws Exception {
+        response = getResponse(pathParameter);
+        try {
+            return new BuiltYear(response.jsonPath().get("page"), response.jsonPath().get("pageSize"),
+                    response.jsonPath().get("totalPageCount"), response.jsonPath().get("wkda"));
+        } catch (ClassCastException e) {
+            throw new Exception("Incorrect data displayed while trying to fetch Car Built years using parameter " + pathParameter);
+        }
+
+    }
+
+    public List<String> getCarMainTypesInList(Map<String, String> wkda) {
+        List<String> allCarTypes = new ArrayList<>();
+        for (Map.Entry<String, String> entry : wkda.entrySet()) {
+            allCarTypes.add(entry.getKey());
+        }
+        return allCarTypes;
+    }
+
+    public Response getResponse(String pathParameter) throws Exception {
+        RestAssured.baseURI = baseURI;
+        requestSpec = RestAssured.given();
+        response = requestSpec.request(Method.GET, pathParameter);
+        if (validateStatusCode(response, pathParameter)) {
+            return response;
+        } else {
+            return null;
+        }
+    }
+
+    public boolean validateStatusCode(Response response, String parameter) throws Exception {
+        int statusCode = response.getStatusCode();
+        if (statusCode == 200) {
+            return true;
+        } else {
+            throw new Exception("Retrieved response StatusCode - " + statusCode + " and error - " + response.jsonPath().get("error").toString()
+                    + ". Please look into parameter - " + parameter);
+        }
+    }
+}
---
+++ src/main/java/stepDefinitions/CarTypes.java
@@ -0,0 +1,37 @@
+package stepDefinitions;
+
+import java.util.Map;
+
+public class CarTypes {
+
+
+    private Integer page;
+    private Integer pageSize;
+    private Integer totalPageCount;
+    private Map<String, String> wkda;
+
+    public CarTypes(Integer page, Integer pageSize, Integer totalPageCount, Map<String, String> wkda) {
+        this.page = page;
+        this.pageSize = pageSize;
+        this.totalPageCount = totalPageCount;
+        this.wkda = wkda;
+    }
+
+    public Integer getPage() {
+        return page;
+    }
+
+    public Integer getPageSize() {
+        return pageSize;
+    }
+
+    public Integer getTotalPageCount() {
+        return totalPageCount;
+    }
+
+    public Map<String, String> getWkda() {
+        return wkda;
+    }
+
+
+}
